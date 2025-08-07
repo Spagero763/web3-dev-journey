3071069e82c58ecc0976f87294fb279ea953cbaa
afolabi ayomide <afolabiayomide870@gmail.com>
2025-08-07 18:33:30 +0000

    ✅ Step 1: _app.tsx — Wagmi + RainbowKit Setup This will:
    Wrap the app w

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,9 +1,9 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { sepolia } from "wagmi/chains";
+import { baseSepolia } from "wagmi/chains";
 
 export const config = getDefaultConfig({
-  appName: "Web3 Dev Journey",
+  appName: "DevHub",
   projectId: "a67439a3f6f1a8b13919e535a8286f56", // Replace with your WalletConnect Project ID
-  chains: [sepolia],
+  chains: [baseSepolia],
   ssr: true,
 });
