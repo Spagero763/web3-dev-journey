43be1ccacc97bdaa10e82508e8734098e6f8127d
afolabi ayomide <afolabiayomide870@gmail.com>
2025-08-07 18:44:27 +0000

    ✅ 10. Optional Styling (Tailwind) Ensure you have the following Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,8 @@ import type {Config} from 'tailwindcss';
 export default {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
