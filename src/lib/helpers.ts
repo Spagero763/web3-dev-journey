f099ff6cf9c9496c3da524512c9d02db19111086
Spagero763 <spagero763@users.noreply.github.com>
2026-01-29 20:51:47 +0100

    feat: add utility helper functions

---
+++ src/lib/helpers.ts
@@ -0,0 +1,31 @@
+﻿export function formatDate(date: Date): string {
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+  }).format(date);
+}
+
+export function truncateAddress(address: string, chars = 4): string {
+  if (!address) return '';
+  return \\...\\;
+}
+
+export function formatNumber(num: number): string {
+  return new Intl.NumberFormat('en-US').format(num);
+}
+
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeout: NodeJS.Timeout;
+  return (...args: Parameters<T>) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), wait);
+  };
+}
+
+export function sleep(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
